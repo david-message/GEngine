e562c3cbf6faea36e08dfc9dac60a40a76ecdfb7
David <david.message@gmail.com>
2018-06-20 12:41:56 +0800

    增加 Assert

--- core/src/main/java/lightlib/sdf/gengine/plugin/Assert.java
+++ core/src/main/java/lightlib/sdf/gengine/plugin/Assert.java
@@ -24,7 +24,35 @@ class AssertOP {
     AssertOP() {
     }
 
-    public void isTrue(boolean condition) {
-        ContextSnapshot.getContext().log("assert.isTrue", condition);
+    public void fail(String message) throws GEngineException {
+        ContextSnapshot.getContext().log("assert.fail", message);
+        throw new GEngineException("assert.fail:" + message);
+    }
+
+    public void isFalse(String message, boolean condition) throws GEngineException {
+        ContextSnapshot.getContext().log("assert.isFalse fail", message);
+        throw new GEngineException("assert.isFalse fail:" + message);
+    }
+
+    public void isTrue(String message, boolean condition) throws GEngineException {
+        ContextSnapshot.getContext().log("assert.isTrue fail", message);
+        throw new GEngineException("assert.isTrue fail:" + message);
+    }
+
+    public void isNotNull(String message, Object object) throws GEngineException {
+        isTrue(message, object != null);
+    }
+
+    public void isNull(String message, Object object) throws GEngineException {
+        isTrue(message, object == null);
+    }
+
+    public void assertEquals(String message, Object expected, Object actual) throws GEngineException {
+        if (expected != null || actual != null) {
+            if (expected == null || !expected.equals(actual)) {
+                ContextSnapshot.getContext().log("assert.assertEquals fail", message);
+                throw new GEngineException("assert.isTrue fail:" + message);
+            }
+        }
     }
 }
